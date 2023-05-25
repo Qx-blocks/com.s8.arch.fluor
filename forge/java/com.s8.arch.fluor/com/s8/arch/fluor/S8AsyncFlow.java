c679f1a84c7d5834ceedf1293513e316650a9121
Pierre Convert <pierreconvert@macbook-pro-6.home>
2023-05-25 16:28:21 +0200

    Update

--- forge/java/com.s8.arch.fluor/com/s8/arch/fluor/S8AsyncFlow.java
+++ forge/java/com.s8.arch.fluor/com/s8/arch/fluor/S8AsyncFlow.java
@@ -1,5 +1,7 @@
 package com.s8.arch.fluor;
 
+import java.util.List;
+
 /**
  * 
  * @author pierreconvert
@@ -27,6 +29,73 @@ public interface S8AsyncFlow {
 	 * @param runnable
 	 */
 	public abstract S8AsyncFlow then(int force, S8CodeBlock runnable);
+	
+
+	public abstract S8User getMe();
+	
+	
+	/**
+	 * 
+	 * @param username
+	 * @param user
+	 * @param onException
+	 */
+	public abstract void getUser(String username, S8ResultProcessor<S8User> user, S8ExceptionCatcher onException);
+	
+	
+	/**
+	 * 
+	 * @param user
+	 * @param onException
+	 */
+	public abstract void scanUsers(S8Filter<S8User> filter, S8ResultProcessor<List<S8User>> user, S8ExceptionCatcher onException);
+	
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public default String getMySpaceId() {
+		return getMe().getPersonalSpaceId();
+	}
+	
+	
+	
+	/**
+	 * 
+	 * @param workspaceId
+	 * @param onAccessed
+	 * @param onException
+	 */
+	public abstract void accessMySpace(S8ResultProcessor<Object[]> onAccessed, S8ExceptionCatcher onException);
+	
+	
+	/**
+	 * 
+	 * @param exposure
+	 * @param onRebased
+	 * @param onException
+	 */
+	public abstract void exposeMySpace(Object[] exposure, S8ResultProcessor<Long> onRebased, S8ExceptionCatcher onException);
+	
+	
+	/**
+	 * 
+	 * @param spaceId
+	 * @param onAccessed
+	 * @param onException
+	 */
+	public abstract void accessSpace(String spaceId, S8ResultProcessor<Object[]> onAccessed, S8ExceptionCatcher onException);
+	
+	
+	/**
+	 * 
+	 * @param exposure
+	 * @param onRebased
+	 * @param onException
+	 */
+	public abstract void exposeSpace(String spaceId, Object[] exposure, S8ResultProcessor<Long> onRebased, S8ExceptionCatcher onException);
 	
 	
 	/**
---
+++ forge/java/com.s8.arch.fluor/com/s8/arch/fluor/S8User.java
@@ -0,0 +1,19 @@
+package com.s8.arch.fluor;
+
+public interface S8User {
+
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getUsername();
+	
+
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getPersonalSpaceId();
+}
