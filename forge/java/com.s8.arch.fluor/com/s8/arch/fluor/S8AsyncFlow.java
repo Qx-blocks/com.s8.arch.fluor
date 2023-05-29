59e7c5a2520371b3da5a07c3808f92c5d3deca6e
Pierre Convert <pierreconvert@localhost>
2023-05-29 12:36:07 +0200

    Update of the flow API

--- forge/java/com.s8.arch.fluor/com/s8/arch/fluor/S8AsyncFlow.java
+++ forge/java/com.s8.arch.fluor/com/s8/arch/fluor/S8AsyncFlow.java
@@ -9,48 +9,10 @@ import java.util.List;
  */
 public interface S8AsyncFlow {
 	
-	
-	
-
-	
-	
-	/**
-	 * 
-	 * @param profile
-	 * @param runnable
-	 */
-	public abstract S8AsyncFlow prior(int force, S8CodeBlock runnable);
-	
-	
-	
-	/**
-	 * 
-	 * @param profile
-	 * @param runnable
-	 */
-	public abstract S8AsyncFlow then(int force, S8CodeBlock runnable);
-	
 
 	public abstract S8User getMe();
 	
-	
-	/**
-	 * 
-	 * @param username
-	 * @param user
-	 * @param onException
-	 */
-	public abstract void getUser(String username, S8ResultProcessor<S8User> user, S8ExceptionCatcher onException);
-	
-	
-	/**
-	 * 
-	 * @param user
-	 * @param onException
-	 */
-	public abstract void scanUsers(S8Filter<S8User> filter, S8ResultProcessor<List<S8User>> user, S8ExceptionCatcher onException);
-	
-	
+
 	
 	/**
 	 * 
@@ -62,13 +24,51 @@ public interface S8AsyncFlow {
 	
 	
 	
+	
+	
+	/**
+	 * 
+	 * @param profile
+	 * @param runnable
+	 */
+	public abstract S8AsyncFlow runBlock(int force, S8CodeBlock runnable);
+
+	
+	
+	/**
+	 * 
+	 * @param username
+	 * @param user
+	 * @param onException
+	 * @return 
+	 */
+	public abstract S8AsyncFlow getUser(String username, 
+			S8ResultProcessor<S8User> user, 
+			S8ExceptionCatcher onException);
+	
+	
+	/**
+	 * 
+	 * @param user
+	 * @param onException
+	 * @return 
+	 */
+	public abstract S8AsyncFlow selectUsers(S8Filter<S8User> filter, 
+			S8ResultProcessor<List<S8User>> user, 
+			S8ExceptionCatcher onException);
+	
+	
+	
 	/**
 	 * 
 	 * @param workspaceId
 	 * @param onAccessed
 	 * @param onException
+	 * @return 
 	 */
-	public abstract void accessMySpace(S8ResultProcessor<Object[]> onAccessed, S8ExceptionCatcher onException);
+	public abstract S8AsyncFlow accessMySpace(
+			S8ResultProcessor<Object[]> onAccessed, 
+			S8ExceptionCatcher onException);
 	
 	
 	/**
@@ -76,8 +76,11 @@ public interface S8AsyncFlow {
 	 * @param exposure
 	 * @param onRebased
 	 * @param onException
+	 * @return 
 	 */
-	public abstract void exposeMySpace(Object[] exposure, S8ResultProcessor<Long> onRebased, S8ExceptionCatcher onException);
+	public abstract S8AsyncFlow exposeMySpace(Object[] exposure, 
+			S8ResultProcessor<Long> onRebased, 
+			S8ExceptionCatcher onException);
 	
 	
 	/**
@@ -85,8 +88,11 @@ public interface S8AsyncFlow {
 	 * @param spaceId
 	 * @param onAccessed
 	 * @param onException
+	 * @return 
 	 */
-	public abstract void accessSpace(String spaceId, S8ResultProcessor<Object[]> onAccessed, S8ExceptionCatcher onException);
+	public abstract S8AsyncFlow accessSpace(String spaceId, 
+			S8ResultProcessor<Object[]> onAccessed, 
+			S8ExceptionCatcher onException);
 	
 	
 	/**
@@ -94,18 +100,20 @@ public interface S8AsyncFlow {
 	 * @param exposure
 	 * @param onRebased
 	 * @param onException
+	 * @return 
 	 */
-	public abstract void exposeSpace(String spaceId, Object[] exposure, S8ResultProcessor<Long> onRebased, S8ExceptionCatcher onException);
+	public abstract S8AsyncFlow exposeSpace(String spaceId, Object[] exposure, 
+			S8ResultProcessor<Long> onRebased, 
+			S8ExceptionCatcher onException);
 	
 	
 	/**
 	 * 
 	 * @param pre
 	 * @param post
+	 * @return 
 	 */
-	public abstract void commit(String repositoryAddress, 
-			String branchId, 
-			Object[] objects,
+	public abstract S8AsyncFlow commit(String repositoryAddress, String branchId, Object[] objects,
 			S8ResultProcessor<Long> onCommitted, 
 			S8ExceptionCatcher onException);
 	
@@ -115,8 +123,7 @@ public interface S8AsyncFlow {
 	 * @param pre
 	 * @param post
 	 */
-	public abstract void cloneHead(String repositoryAddress, 
-			String branchId, 
+	public abstract S8AsyncFlow cloneHead(String repositoryAddress, String branchId, 
 			S8ResultProcessor<Object[]> onCloned, 
 			S8ExceptionCatcher onException);
 	
@@ -126,15 +133,22 @@ public interface S8AsyncFlow {
 	 * @param pre
 	 * @param post
 	 */
-	public abstract void cloneVersion(
-			String repositoryAddress, 
-			String branchId, 
-			long version,
+	public abstract S8AsyncFlow cloneVersion(String repositoryAddress, String branchId, long version,
 			S8ResultProcessor<Object[]> onCloned, 
 			S8ExceptionCatcher onException);
 	
 	
+	
+	/**
+	 * Send and play 
+	 */
 	public void send();
 	
+	
+	
+	/**
+	 * 
+	 */
+	public void play();
 	
 }
